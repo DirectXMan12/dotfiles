1533e7c53ad2cfa43efe1e2cec99224cf554fe03
Solly Ross <directxman12+gh@gmail.com>
2024-11-05 13:31:31 -0800

    [neovim] work around "server cancelled request" lsp error
    See also https://github.com/neovim/neovim/issues/30985.
    Just manually ignore them till a fix is merged.

--- includes/neovim.nix
+++ includes/neovim.nix
@@ -213,6 +213,18 @@ in
 							},
 						},
 					}
+
+					-- work around github:neovim/neovim#30985 until
+					-- github:neovim/neovim#30999 is merged
+					for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+						local default_diagnostic_handler = vim.lsp.handlers[method]
+						vim.lsp.handlers[method] = function(err, result, context, config)
+							if err ~= nil and err.code == -32802 then
+								return
+							end
+							return default_diagnostic_handler(err, result, context, config)
+						end
+					end
 				'';
 			}
 			{
